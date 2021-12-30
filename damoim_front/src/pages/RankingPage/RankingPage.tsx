2bce3cda61742e02b6ad39528233c03b4f8453a2
Chanhyuk Park <pgg6713@gmail.com>
2021-12-30 17:14:41 +0900

    [add] ranking page halftextarea

--- damoim_front/src/pages/RankingPage/RankingPage.tsx
+++ damoim_front/src/pages/RankingPage/RankingPage.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {RankingPageContainer} from "./rankingPageStyles";
 import OTTSelectBar from "../../components/OTTSelectBar/OTTSelectBar";
 import SmallGuide from "../../components/SmallGuide/SmallGuide";
+import HalfTextArea from "../../components/HalfTextArea/HalfTextArea";
 const initialSelectedOTTs = ["netflix"]
 
 function RankingPage(props: any) {
@@ -9,6 +10,8 @@ function RankingPage(props: any) {
 
     return (
         <RankingPageContainer>
+            <HalfTextArea title={"순위"} content={"OTT별 오늘의 영상 순위를 확인할 수 있습니다😘"}/>
+
             <OTTSelectBar selectedOTTs={selectedOTTs} setSelectedOTTs={setSelectedOTTs} selectOnlyOne/>
             <SmallGuide content={"아이콘을 클릭하여 원하는 정보를 확인하세요"}/>
 
