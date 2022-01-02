a6210b20ab60d919cc578031d8a4b228ea79abd9
Chanhyuk Park <pgg6713@gmail.com>
2022-01-02 16:30:38 +0900

    [fix] community detail responsive for mobile

--- damoim_front/src/components/CommunityPostDetail/communityPostDetailStyles.tsx
+++ damoim_front/src/components/CommunityPostDetail/communityPostDetailStyles.tsx
@@ -5,4 +5,11 @@ export const CommunityPostDetailContainer = styled.div`
   span{
     color: #777;
   }
+
+  @media screen and (max-width: 480px) {
+    .post-created-date{
+      font-size: 0.6rem;
+    }
+  }
+  
 `
