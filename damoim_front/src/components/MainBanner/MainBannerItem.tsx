0be4314d90d354dc2fd5445a48be7a33bb6b1042
Chanhyuk Park <pgg6713@gmail.com>
2022-01-15 23:07:46 +0900

    [add] mobile / web render different main banner

--- damoim_front/src/components/MainBanner/MainBannerItem.tsx
+++ damoim_front/src/components/MainBanner/MainBannerItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
 import MainBanner from './MainBanner';
 
@@ -16,25 +16,62 @@ const SliderItem = styled.div`
   }
 `;
 
-const items:itemsProps[] = [
-  {
-    item: '/images/mainBanner/mainBanner1.png',
-    name: '이미지01'
-  },
-  {
-    item: '/images/mainBanner/mainBanner2.png',
-    name: '이미지02'
-  },
-  {
-    item: '/images/mainBanner/mainBanner3.png',
-    name: '이미지03'
-  },    
-]
+
 
 function Item() {
+
+  const [width,setWidth] = useState(window.innerWidth) ;
+  const [items,setItems] = useState<itemsProps[]>();
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize',handleResize);
+    return () => {
+      window.removeEventListener('resize',handleResize)
+    }
+  })
+
+
+  useEffect(()=>{
+    if(width < 500){
+      setItems( [
+        {
+          item: '/images/mainBanner/mainBanner1-mobile.png',
+          name: '이미지01'
+        },
+        {
+          item: '/images/mainBanner/mainBanner2-mobile.png',
+          name: '이미지02'
+        },
+        {
+          item: '/images/mainBanner/mainBanner3-mobile.png',
+          name: '이미지03'
+        },
+      ])
+    }else{
+      setItems( [
+        {
+          item: '/images/mainBanner/mainBanner1.png',
+          name: '이미지01'
+        },
+        {
+          item: '/images/mainBanner/mainBanner2.png',
+          name: '이미지02'
+        },
+        {
+          item: '/images/mainBanner/mainBanner3.png',
+          name: '이미지03'
+        },
+      ])
+    }
+
+  },[width])
+
   return (
     <MainBanner>
-      {items.map((item, index) => (
+      {items?.map((item, index) => (
         <SliderItem key={index}>
           <img src={item.item} alt={item.name} />
         </SliderItem>
