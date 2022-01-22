8366b75a3607aac7d420505ca850847a07a9c58e
Chanhyuk Park <pgg6713@gmail.com>
2022-01-22 18:45:17 +0900

    [fix] loading styles

--- damoim_front/src/components/Loading/loadingStyles.tsx
+++ damoim_front/src/components/Loading/loadingStyles.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const LoadingArea = styled.div`
-  margin: 400px auto 300px;
-  width: 1200px;
-  text-align: center;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
